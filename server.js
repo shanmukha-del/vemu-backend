fc0eca7444cbf3221359ba1b4c83dc6c58aff7de
shanmukha-del <ksavithri462@gmail.com>
2026-04-18 15:49:05 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
 app.use(express.json());
 
 // 1. Database Connection Logic
-const PORT = 3000; 
+const PORT = 5000; 
 
 const MONGO_URI = 'mongodb://vemuadmin:vemu123@ac-tp832eg-shard-00-00.w4je3f4.mongodb.net:27017,ac-tp832eg-shard-00-01.w4je3f4.mongodb.net:27017,ac-tp832eg-shard-00-02.w4je3f4.mongodb.net:27017/vemu_attendance?ssl=true&replicaSet=atlas-zbds82-shard-0&authSource=admin&retryWrites=true&w=majority';
 
